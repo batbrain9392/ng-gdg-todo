19055ab23e88651918cf766a5acac240126087be
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-10-28 12:23:37 +0530

    fix(todo): removed details comp for now

--- src/app/feature/todos/pages/index.ts
+++ src/app/feature/todos/pages/index.ts
@@ -1,13 +1,11 @@
 import { AddEditComponent } from './list/components/add-edit/add-edit.component';
 import { ListComponent } from './list/list.component';
-import { DetailsComponent } from './details/details.component';
 
 export {
   AddEditComponent
 } from './list/components/add-edit/add-edit.component';
 export { ListComponent } from './list/list.component';
-export { DetailsComponent } from './details/details.component';
 
-export const pages = [AddEditComponent, ListComponent, DetailsComponent];
+export const pages = [AddEditComponent, ListComponent];
 
 export const entryComponents = [AddEditComponent];
