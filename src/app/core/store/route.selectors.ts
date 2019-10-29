47582c91c58c5f1da733b20b850e97e1c5e13629
batbrain9392 <debmallyabhattacharya@gmail.com>
2019-10-29 17:55:28 +0530

    feat(route): added selectors

--- src/app/core/store/route.selectors.ts
+++ src/app/core/store/route.selectors.ts
@@ -1,9 +1,38 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { RouterReducerState } from '@ngrx/router-store';
 import { RouterStateUrl } from './route.serializer';
 import * as fromApp from './index';
 
-export const selectRouterState = createFeatureSelector<
+const selectRouterState = createFeatureSelector<
   fromApp.State,
   RouterReducerState<RouterStateUrl>
 >(fromApp.routerKey);
+
+export const selectQueryParams = createSelector(
+  selectRouterState,
+  routerState => routerState.state.queryParams
+);
+export const selectQueryParam = (param: string) =>
+  createSelector(
+    selectRouterState,
+    routerState => routerState.state.queryParams[param]
+  );
+export const selectRouteParams = createSelector(
+  selectRouterState,
+  routerState => routerState.state.params
+);
+export const selectRouteParam = (param: string) =>
+  createSelector(
+    selectRouterState,
+    routerState => routerState.state.params[param]
+  );
+// export const selectRouteData = createSelector(
+//   selectRouterState,
+//   routerState => routerState.state
+// );
+export const selectUrl = createSelector(
+  selectRouterState,
+  routerState => routerState.state.url
+);
+
+export const selectReturnUrl = selectQueryParam('returnUrl');
